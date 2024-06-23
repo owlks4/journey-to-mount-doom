9ca446e74fe32f6745c9f5dcd8498c2eea3626ab
owlks4 <115742200+owlks4@users.noreply.github.com>
2024-06-23 20:10:37 +0100

    Update page route

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://owlks4.github.io/the-longest-way/",
+  base: "https://owlks4.github.io/journey-to-mount-doom/",
   assetsInclude:"**/*.kml"
 })
